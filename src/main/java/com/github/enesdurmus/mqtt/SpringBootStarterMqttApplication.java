027d6f85469d53dc4825d58ae4701c6c15e13c29
enesdurmus <enesdurmus3738@gmail.com>
2025-09-19 21:49:56 +0300

    init

---
+++ src/main/java/com/github/enesdurmus/mqtt/SpringBootStarterMqttApplication.java
@@ -0,0 +1,13 @@
+package com.github.enesdurmus.mqtt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootStarterMqttApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootStarterMqttApplication.class, args);
+	}
+
+}
